3674e894ee313eaa67293b82fe459bf5d15d81da
Nexo Options <nexonexo712@gmail.com>
2025-09-22 00:29:00 +0300

    Create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,53 @@
+import {HardhatUserConfig} from "hardhat/types"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@typechain/hardhat"
+import "hardhat-deploy"
+import "hardhat-abi-exporter"
+import "hardhat-deploy-ethers"
+import "hardhat-docgen"
+import "hardhat-gas-reporter"
+import "hardhat-watcher"
+import "hardhat-local-networks-config-plugin"
+import "solidity-coverage"
+import {config as dotEnvConfig} from "dotenv"
+
+dotEnvConfig()
+
+const {ETHERSCAN_API_KEY, COIN_MARKET_CAP} = process.env
+
+const config: HardhatUserConfig = {
+  localNetworksConfig: "~/.hardhat/networks.json",
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.6",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+      {
+        version: "0.6.6",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+      {
+        version: "0.5.16",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+    ],
+  },
+  networks: {
+    coverage: {
+      url: "http://127.0.0.1:8555",
+    },
+    hlocal: {
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic:
+          "myth like bonus scare over problem client lizard pioneer submit female collect",
+      },
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
